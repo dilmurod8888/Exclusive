b0f9cce8339025c73b1c5b2ba48f807d49d3e5e2
dilmurod8888 <dilmurodsalimjonov8888@gmail.com>
2024-07-31 18:28:32 +0500

    dilmurodEtiborgit add .git add .!

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ let swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
